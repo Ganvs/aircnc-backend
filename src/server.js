faa0f0ab316cfae2994ea76e7c5e6160491414ee
Ganvs <gustavo.anverze@gmail.com>
2019-10-05 09:42:32 -0300

    socketio added

--- src/server.js
+++ src/server.js
@@ -3,9 +3,18 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
+const socketio = require("./routes");
+const http = require("http");
+
 const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on("connection", socket => {
+  console.log("Usuário conectado", socket.id);
+});
 
 mongoose.connect(
   "mongodb+srv://anverze:anverze@cluster0-7n7zd.mongodb.net/aircnc?retryWrites=true&w=majority",
@@ -20,4 +29,5 @@ app.use(express.json());
 
 app.use(routes);
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
-app.listen(3333);
+
+server.listen(3333);
